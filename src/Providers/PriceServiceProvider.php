17a87723964b2aa7ae442707a942476bc73c87fd
railken <railken@protonmail.ch>
2019-03-18 03:31:22 +0100

    add macro relationships

--- src/Providers/PriceServiceProvider.php
+++ src/Providers/PriceServiceProvider.php
@@ -16,4 +16,14 @@ class PriceServiceProvider extends CommonServiceProvider
         $this->app->register(\Railken\Amethyst\Providers\ConsumeRuleServiceProvider::class);
         $this->app->register(\Railken\Amethyst\Providers\TargetServiceProvider::class);
     }
+
+    public function boot()
+    {
+        \Illuminate\Database\Eloquent\Builder::macro('price', function () {
+            return $this->getModel()->morphOne(\Railken\Amethyst\Models\Price::class, 'priceable');
+        });
+        \Illuminate\Database\Eloquent\Builder::macro('prices', function () {
+            return $this->getModel()->morphMany(\Railken\Amethyst\Models\Price::class, 'priceable');
+        });
+    }
 }
