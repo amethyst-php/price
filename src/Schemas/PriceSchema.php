200554ad5717d632f4d6818b94bf3776a8f1eda3
Railken <railken@protonmail.ch>
2020-02-18 19:15:49 +0100

    refactor morphto

--- src/Schemas/PriceSchema.php
+++ src/Schemas/PriceSchema.php
@@ -30,11 +30,11 @@ class PriceSchema extends Schema
                 ->setRelationName('target')
                 ->setRelationManager(TargetManager::class),
             Attributes\NumberAttribute::make('price')->setRequired(true),
-            Attributes\EnumAttribute::make('priceable_type', app('amethyst')->getMorphListable('price', 'priceable')),
+            Attributes\EnumAttribute::make('priceable_type', app('amethyst')->getDataNames()),
             Attributes\MorphToAttribute::make('priceable_id')
                 ->setRelationKey('priceable_type')
                 ->setRelationName('priceable')
-                ->setRelations(app('amethyst')->getMorphRelationable('price', 'priceable')),
+                ->setRelations(app('amethyst')->getDataManagers()),
             Attributes\CreatedAtAttribute::make(),
             Attributes\UpdatedAtAttribute::make(),
             Attributes\DeletedAtAttribute::make(),
